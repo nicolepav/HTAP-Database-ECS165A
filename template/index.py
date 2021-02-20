c00a53d7dafdbce5cebf63d6e18fe9e24b30604c
Steven Tan <steventangerine@gmail.com>
2021-02-19 19:46:59 -0800

    Tree implemented

--- template/index.py
+++ template/index.py
@@ -9,6 +9,7 @@ class Index:
     def __init__(self, table):
         # One index for each table. All our empty initially.
         self.indices = [None] *  table.num_columns
+        print(self.indices)
         pass
 
     """
@@ -30,6 +31,7 @@ class Index:
     """
 
     def create_index(self, column_number):
+
         pass
 
     """
@@ -38,3 +40,81 @@ class Index:
 
     def drop_index(self, column_number):
         pass
+
+
+
+class Node:
+    def __init__(self, data = None):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        # check if root node is none
+        if self.root is None:
+            self.root = Node(data)
+        #tree has at least one node in it and find appro location to put the new node
+        #create a helper method _insert
+        else:
+            self._insert(data, self.root)
+
+    def _insert(self, data, cur_node):
+        #data is less than cur_node data
+        if data < cur_node.data:
+            #left node is avaliable for insert
+            if cur_node.left is None:
+                cur_node.left = Node(data)
+            #else traverse down
+            else:
+                self._insert(data,cur_node.left)
+        elif data >= cur_node.data:
+            if cur_node.right is None:
+                cur_node.right = Node(data)
+            else:
+                self._insert(data, cur_node.right)
+
+    def findLocation(self, value):
+        
+        pass
+
+    def Print(self, cur_node, begin, end):
+     
+    # Base Case Start at root
+        if cur_node is None:
+            return
+ 
+    # Since the desired o/p is sorted, recurse for left
+    # subtree first. If root.data is greater than k1, then
+    # only we can get o/p keys in left subtree
+        if begin < cur_node.data :
+            self.Print(cur_node.left, begin, end)
+ 
+    # If root's data lies in range, then prints root's data
+        if begin <= cur_node.data and end >= cur_node.data:
+            print (cur_node.data)
+ 
+    # If root.data is smaller than k2, then only we can get
+    # o/p keys in right subtree
+        if end > cur_node.data:
+            self.Print(cur_node.right, begin, end)
+            
+
+
+
+
+
+
+    def print_tree(self, traversal_type):
+        if traversal_type == "preorder":
+            return self.preorder_print(self.root, "")
+
+    def preorder_print(self,start,traversal):
+        if start:
+            traversal += (str(start.data) + "-")
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
+        return traversal
---
+++ tester.py
@@ -0,0 +1,18 @@
+from template.index import *
+
+
+
+tree = BST()
+
+tree.insert(3)
+tree.insert(6)
+tree.insert(1)
+tree.insert(2)
+tree.insert(78)
+tree.insert(3)
+tree.insert(9)
+tree.insert(2)
+tree.insert(6)
+tree.insert(3)
+
+tree.Print(tree.root, 3, 10)
