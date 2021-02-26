d5b2a61fe235b8aac47531f3a99e64188d9558cd
Steven Tan <steventangerine@gmail.com>
2021-02-25 17:49:30 -0800

    Btree works with records now

--- indextest.py
+++ indextest.py
@@ -3,11 +3,19 @@ from template.index import *
 
 tree = BTree()
 
-tree.insert([0,0,0,0,0], 3)
+tree.insert([0,13,0,0,0],2)
+tree.insert([0,7,0,0,0],2)
+tree.insert([0,24,0,0,0],2)
+tree.insert([0,15,0,0,0],2)
+tree.insert([0,4,0,0,0],2)
+tree.insert([0,29,0,0,0],2)
+tree.insert([0,20,0,0,0],2)
+tree.insert([0,16,0,0,0],2)
+tree.insert([0,19,0,0,0],2)
+tree.insert([0,1,0,0,0],2)
+tree.insert([0,5,0,0,0],2)
+tree.insert([0,22,0,0,0],2)
+tree.insert([0,17,0,0,0],2)
 
-tree.insert([33,1,1,1,1], 3)
-tree.insert([22,2,2,2,2], 3)
-tree.insert([332,3,3,3,3], 3)
-tree.insert([3321,4,3,3,3], 3)
-
-tree.preorder()
+tree.printTop2Tiers()
+print (tree.find(16, 2))
--- template/index.py
+++ template/index.py
@@ -55,134 +55,59 @@ class Index:
 
 
 
-# class Node:
-#     def __init__(self, data = None, record = None):
-#         self.data = data
-#         self.record = record #pointer
-#         self.left = None
-#         self.right = None
-
-# class BST:
-#     def __init__(self):
-#         self.root = None
-#         self.returnData = []
-
-#     def insert(self, data, record):
-#         # check if root node is none
-#         if self.root is None:
-#             self.root = Node(data, record)
-#         #tree has at least one node in it and find appro location to put the new node
-#         #create a helper method _insert
-#         else:
-#             self._insert(data, record, self.root)
-
-#     def _insert(self, data, record, cur_node):
-#         #data is less than cur_node data
-#         if data < cur_node.data:
-#             #left node is avaliable for insert
-#             if cur_node.left is None:
-#                 cur_node.left = Node(data, record)
-#             #else traverse down
-#             else:
-#                 self._insert(data, record, cur_node.left)
-#         elif data >= cur_node.data:
-#             if cur_node.right is None:
-#                 cur_node.right = Node(data , record)
-#             else:
-#                 self._insert(data, record, cur_node.right)
-
-#     # TODO change append data to record when data insertion is figured out
-#     def returnRangeData(self, cur_node, begin, end):
-        
-#     # Base Case Start at root
-        
-#         if cur_node is None:
-#             return
- 
-#     # Since the desired o/p is sorted, recurse for left
-#     # subtree first. If root.data is greater than k1, then
-#     # only we can get o/p keys in left subtree
-#         if begin < cur_node.data :
-#             self.returnRangeData(cur_node.left, begin, end)
- 
-#     # If root's data lies in range, then prints root's data
-#         if begin <= cur_node.data and end >= cur_node.data:
-#             self.returnData.append(cur_node.record[0])
- 
-#     # If root.data is smaller than k2, then only we can get
-#     # o/p keys in right subtree
-#         if end >= cur_node.data:
-#             self.returnRangeData(cur_node.right, begin, end)
-
-#     def clearReturnData(self):
-#         self.returnData = []
-
-
-
-
-#     def print_tree(self, traversal_type):
-#         if traversal_type == "preorder":
-#             return self.preorder_print(self.root, "")
-
-#     def preorder_print(self,start,traversal):
-#         if start:
-#             traversal += (str(start.data) + "-")
-#             traversal = self.preorder_print(start.left, traversal)
-#             traversal = self.preorder_print(start.right, traversal)
-#         return traversal
-
-
-
-
-
 class BNode:
-    def __init__(self, record, par = None):
-        # each node will have a list of record objects within it
-        self.records = list([record])
+    def __init__(self, data, par=None):
+        #print ("Node __init__: " + str(data))
+        self.data = list([data])
         self.parent = par
         self.child = list()
-    #tester to print out node data
+
     def __str__(self):
         if self.parent:
-            return str(self.parent.records) + ' : ' + str(self.records)
-        return "Root: " + str(self.records)
+            return str(self.parent.data) + ' : ' + str(self.data)
+        return 'Root : ' + str(self.data)
+
+    def __lt__(self, node):
+        return self.data[0] < node.data[0]
 
     def _isLeaf(self):
         return len(self.child) == 0
 
+    # merge new_node sub-tree into self node
     def _add(self, new_node):
+        # print ("Node _add: " + str(new_node.data) + ' to ' + str(self.data))
         for child in new_node.child:
             child.parent = self
-
-        self.records.extend(new_node.records)
-        self.records.sort()
+        self.data.extend(new_node.data)
+        self.data.sort()
         self.child.extend(new_node.child)
         if len(self.child) > 1:
             self.child.sort()
-            for child in self.child:
-                child.parent = self
+        if len(self.data) > 2:
+            self._split()
 
-            if len(self.records) > 3:
-                self._split()
-        
-
+    # find correct node to insert new node into tree
     def _insert(self, new_node, keyColumn):
-        #if the node is a leaf: add record to leaf and rebalance tree
+        # print ('Node _insert: ' + str(new_node.data) + ' into ' + str(self.data))
+
+        # leaf node - add data to leaf and rebalance tree
         if self._isLeaf():
             self._add(new_node)
-        # not leaf: find correct child to descend and do recursive insert
-        elif new_node.record[0][keyColumn-1] > self.records[-1][keyColumn-1]:
+
+        # not leaf - find correct child to descend, and do recursive insert
+        elif new_node.data[0][keyColumn] > self.data[-1][keyColumn]:
             self.child[-1]._insert(new_node, keyColumn)
         else:
-            for i in range(0, len(self.records)):
-                if new_node.records[0][keyColumn-1] < self.records[i][keyColumn-1]:
+            for i in range(0, len(self.data)):
+                if new_node.data[0][keyColumn] < self.data[i][keyColumn]:
                     self.child[i]._insert(new_node, keyColumn)
                     break
 
+    # 3 items in node, split into new sub-tree and add to parent
     def _split(self):
-        left_child = BNode(self.record[0], self)
-        right_child = BNode(self.record[2], self)
-        #if self is not a leaf, reattach its child with the new node
+        # print("Node _split: " + str(self.data))
+        left_child = BNode(self.data[0], self)
+        right_child = BNode(self.data[2], self)
         if self.child:
             self.child[0].parent = left_child
             self.child[1].parent = left_child
@@ -190,36 +115,77 @@ class BNode:
             self.child[3].parent = right_child
             left_child.child = [self.child[0], self.child[1]]
             right_child.child = [self.child[2], self.child[3]]
+
         self.child = [left_child]
         self.child.append(right_child)
-        #now have new sub-tree, self. needs to add self to its parent node
+        self.data = [self.data[1]]
+
+        # now have new sub-tree, self. need to add self to its parent node
         if self.parent:
             if self in self.parent.child:
                 self.parent.child.remove(self)
+            self.parent._add(self)
         else:
             left_child.parent = self
             right_child.parent = self
 
+    # find an item in the tree; return item, or False if not found
+    def _find(self, key, keyColumn):
+        # print ("Find " + str(item))
+        for record in self.data:
+            if record[keyColumn] == key:
+                return record
+
+        if self._isLeaf():
+        	return False
+        elif key > self.data[-1][keyColumn]:
+        	return self.child[-1]._find(key, keyColumn)
+        else:
+        	for i in range(len(self.data)):
+        		if key < self.data[i][keyColumn]:
+        			return self.child[i]._find(key, keyColumn)
+
+    def _remove(self, item):
+        pass
+
+    # print preorder traversal
     def _preorder(self):
         print(self)
         for child in self.child:
             child._preorder()
+
+
 class BTree:
     def __init__(self):
         print("Tree __init__")
         self.root = None
 
     def insert(self, record, keyColumn):
-        print("Tree insert key: " + str(record[keyColumn-1]))
+        print("Tree insert: " + str(record[keyColumn-1]))
         if self.root is None:
             self.root = BNode(record)
         else:
-            self.root._insert(BNode(record), keyColumn)
+            self.root._insert(BNode(record), keyColumn-1)
             while self.root.parent:
                 self.root = self.root.parent
-
         return True
 
+    # TODO: change to find range of keys
+    def find(self, key, keyColumn):
+        return self.root._find(key, keyColumn-1)
+
+    def remove(self, record):
+        self.root.remove(record)
+
+    def printTop2Tiers(self):
+        print('----Top 2 Tiers----')
+        print(str(self.root.data))
+        for child in self.root.child:
+            print(str(child.data), end=' ')
+        print(' ')
+
     def preorder(self):
-        print("----Preorder----")
+        print('----Preorder----')
         self.root._preorder()
+
+
